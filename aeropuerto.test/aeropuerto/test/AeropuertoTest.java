a7093162a8d7e64d7f7e2d5d4990841695d88e4e
MatiasTortoriello <matiastortoriello1991@gmail.com>
2023-05-10 20:17:07 -0300

    Creación de clases

--- aeropuerto.test/aeropuerto/test/AeropuertoTest.java
+++ aeropuerto.test/aeropuerto/test/AeropuertoTest.java
@@ -7,8 +7,9 @@ import org.junit.Test;
 public class AeropuertoTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void queSeCreeUnVueloYSeAgregueUnPasajero() {
+		
+		
 	}
 
 }
---
+++ src/aeropuerto/dominio/Pasajero.java
@@ -0,0 +1,5 @@
+package aeropuerto.dominio;
+
+public class Pasajero {
+
+}
