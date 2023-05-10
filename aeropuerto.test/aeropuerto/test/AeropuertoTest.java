cedf9320564a76605b7dbb117163fba9d1dd0ab3
MatiasTortoriello <matiastortoriello1991@gmail.com>
2023-05-10 20:14:15 -0300

    Primer commit

---
+++ aeropuerto.test/aeropuerto/test/AeropuertoTest.java
@@ -0,0 +1,14 @@
+package aeropuerto.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AeropuertoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
